154a5098485265dfee7e61297668a5f2ac306f6b
Chung Ho Chiu <ho@glassnode.com>
2024-05-07 17:03:21 -0700

    add sample response to api-requester example

--- example/api-requester/pkg/requester/requester.go
+++ example/api-requester/pkg/requester/requester.go
@@ -44,6 +44,15 @@ func (r Requester) GetJsonPlaceHolder(ctx context.Context) ([]Response, error) {
 	return result, nil
 }
 
+// Sample response from https://jsonplaceholder.typicode.com/posts
+// [
+//  {
+//     "userId": 1,
+//     "id": 1,
+//     "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+//     "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+//   }
+// ]
 type Response struct {
 	UserId int64  `json:"userId"`
 	Id     int64  `json:"id"`
